34e0f05163d7400c7d9bdc64d83b2721e553b3c9
Riga1231 <riga_jericho@plpasig.edu.ph>
2025-07-21 10:36:24 +0800

    first-commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// index.js
+const express = require("express");
+const cors = require("cors");
+const pool = require("./db");
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT NOW() AS `current_time`");
+    res.send(`Connected! Server time is: ${rows[0].current_time}`);
+  } catch (err) {
+    console.error("DB Connection Error:", err);
+    res.status(500).send("Database connection failed");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
